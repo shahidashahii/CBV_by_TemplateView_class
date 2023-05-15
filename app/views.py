22a288d3ad62a353003e69d96ccbad955a288897
shahidashahii <shahidashahi2000@gmail.com>
2023-05-15 19:20:20 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,5 +1,2 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
 # Create your views here.
-class Cbv_html(TemplateView):
-    template_name='Cbv_html.html'
--- project40/urls.py
+++ project40/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import *
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Cbv_html/',Cbv_html.as_view(),name='Cbv_html'),
+    path('Cbv_html/',TemplateView.as_view(template_name='Cbv_html.html'),name='Cbv_html'),
 ]
